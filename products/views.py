829d6d99a220cf9d8fd3b0bac463e7a6449d64c7
dornelesarthur <dorneles.ah@gmail.com>
2023-03-09 20:21:11 -0300

    Config templates and pages

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'products/index.html')
+    return render(request, 'pages/products/index.html')
 
 def view(request):
     pass
